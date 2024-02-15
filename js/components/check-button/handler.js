374810331b76ac4cf8fe78aa3913f337bdbcab19
davidpinkevich <davidpinkevich95@gmail.com>
2024-02-15 21:31:15 +0300

    feat: add handler for check

--- js/components/check-button/handler.js
+++ js/components/check-button/handler.js
@@ -1,8 +1,10 @@
 import { TODO_ITEMS } from "../../data";
+import { getElement } from "../../utils/getElement";
 import check from "../../../assets/icons/check.svg";
 
 function handlerCheckTodo(item, btn, id) {
   const checkIndex = TODO_ITEMS.findIndex((item) => item.id === id);
+  const target = getElement(".filters__target");
 
   if (TODO_ITEMS[checkIndex].completed) {
     btn.innerHTML = "";
@@ -10,6 +12,14 @@ function handlerCheckTodo(item, btn, id) {
   } else {
     btn.innerHTML = `<img src=${check} />`;
     item.classList.toggle("line__through");
+  }
+  if (
+    target.classList.contains("filters__active") ||
+    target.classList.contains("filters__completed")
+  ) {
+    item.classList.add("disabled");
+  } else {
+    item.classList.add("item");
   }
   TODO_ITEMS[checkIndex].completed = !TODO_ITEMS[checkIndex].completed;
 }
